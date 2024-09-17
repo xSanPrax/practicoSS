41e22e089c1f623257f0645addd7ea287846d5a6
xSanPrax <92343442+xSanPrax@users.noreply.github.com>
2024-09-16 22:00:52 -0300

    commit 2

--- Copia de demo2023/demo2023-web/src/main/java/uy/edu/fing/tse/demo2023/beans/VehiculosBean.java
+++ Copia de demo2023/demo2023-web/src/main/java/uy/edu/fing/tse/demo2023/beans/VehiculosBean.java
@@ -1,42 +1,42 @@
-package uy.edu.fing.tse.demo2023.beans;
+// package uy.edu.fing.tse.demo2023.beans;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
-import javax.ejb.EJB;
-import javax.enterprise.context.*;
-import javax.inject.Named;
+// import javax.ejb.EJB;
+// import javax.enterprise.context.*;
+// import javax.inject.Named;
 
-import uy.edu.fing.tse.demo2023.model.Vehiculos;
-import uy.edu.fing.tse.demo2023.service.IVehiculosService;
-import javax.annotation.*;
+// import uy.edu.fing.tse.demo2023.model.Vehiculos;
+// import uy.edu.fing.tse.demo2023.service.IVehiculosService;
+// import javax.annotation.*;
 
-import uy.edu.fing.tse.demo2023.dto.VehiculoDTO;
+// import uy.edu.fing.tse.demo2023.dto.VehiculoDTO;
 
-@Named("vehiculos")
-@RequestScoped
-public class VehiculosBean {
+// @Named("vehiculos")
+// @RequestScoped
+// public class VehiculosBean {
 
-	@EJB
-	IVehiculosService service;
+// 	@EJB
+// 	IVehiculosService service;
 	
-	private Vehiculos vehiculos;
+// 	private Vehiculos vehiculos;
 	
-	private ArrayList<VehiculoDTO> list;
+// 	private ArrayList<VehiculoDTO> list;
 	
-	public VehiculosBean() {
+// 	public VehiculosBean() {
 		
-	}
+// 	}
 	
 
-	@PostConstruct
-	public void init() {
-		vehiculos = service.obtenerVehiculos();
-		list = vehiculos.getListaVehiculos();
-	}
+// 	@PostConstruct
+// 	public void init() {
+// 		vehiculos = service.obtenerVehiculos();
+// 		list = vehiculos.getListaVehiculos();
+// 	}
 	
-	public ArrayList<VehiculoDTO> getList() {
-		return list;
-	}
+// 	public ArrayList<VehiculoDTO> getList() {
+// 		return list;
+// 	}
 
 	
-}
+// }
--- Copia de demo2023/demo2023-web/src/main/java/uy/edu/fing/tse/demo2023/beans/bvVehiculos.java
+++ Copia de demo2023/demo2023-web/src/main/java/uy/edu/fing/tse/demo2023/beans/bvVehiculos.java
@@ -1,44 +1,44 @@
-package uy.edu.fing.tse.demo2023.beans;
+// package uy.edu.fing.tse.demo2023.beans;
 
-import java.io.Serializable;
-import java.util.ArrayList;
+// import java.io.Serializable;
+// import java.util.ArrayList;
 
-import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
-import javax.faces.view.ViewScoped;
-import javax.inject.Named;
+// import javax.annotation.PostConstruct;
+// import javax.ejb.EJB;
+// import javax.faces.view.ViewScoped;
+// import javax.inject.Named;
 
-import uy.edu.fing.tse.demo2023.dto.VehiculoDTO;
-import uy.edu.fing.tse.demo2023.model.Vehiculos;
-import uy.edu.fing.tse.demo2023.service.IVehiculosService;
+// import uy.edu.fing.tse.demo2023.dto.VehiculoDTO;
+// import uy.edu.fing.tse.demo2023.model.Vehiculos;
+// import uy.edu.fing.tse.demo2023.service.IVehiculosService;
 
-@Named("dtBasicView")
-@ViewScoped
-public class bvVehiculos implements Serializable {
+// @Named("dtBasicView")
+// @ViewScoped
+// public class bvVehiculos implements Serializable {
 	
-	private static final long serialVersionUID = 1L;
+// 	private static final long serialVersionUID = 1L;
 	
-	@EJB
-	IVehiculosService service;
+// 	@EJB
+// 	IVehiculosService service;
 	
 
-	private Vehiculos vehiculos;
-	private ArrayList<VehiculoDTO> list;
+// 	private Vehiculos vehiculos;
+// 	private ArrayList<VehiculoDTO> list;
 	
-	@PostConstruct
-	public void init() {
+// 	@PostConstruct
+// 	public void init() {
 		
-		vehiculos = service.obtenerVehiculos();
-		list = vehiculos.getListaVehiculos();
-	}
+// 		vehiculos = service.obtenerVehiculos();
+// 		list = vehiculos.getListaVehiculos();
+// 	}
 	
-	public ArrayList<VehiculoDTO> getVehiculos() {
-		return list;
-	}
+// 	public ArrayList<VehiculoDTO> getVehiculos() {
+// 		return list;
+// 	}
 	
-	public ArrayList<VehiculoDTO> getList() {
-		return list;
-	}
+// 	public ArrayList<VehiculoDTO> getList() {
+// 		return list;
+// 	}
 	
 	
-}
+// }
